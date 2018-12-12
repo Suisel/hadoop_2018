0ac20f383c5775efa1ea25164130508cf52a3476
Elizaveta Lavelina <lavelinae@gmail.com>
2018-12-12 17:59:26 +0300

    Task 1

---
+++ code/mrjob/task1.py
@@ -0,0 +1,20 @@
+from mrjob.job import MRJob
+from mrjob.protocol import TextProtocol
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class MRWordFreqCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield None, [len(word), word.lower()]
+
+
+    def reducer(self, _, length_word):
+        yield max(length_word)
+
+
+if __name__ == '__main__':
+    MRWordFreqCount.run()
